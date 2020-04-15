3187cfbfa211b86f0b7d46bbc92e1c5179aaf190
Denis <denis@efojs.com>
2020-04-16 02:43:43 +0300

    Add basic JWT authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+  respond_to :json
+  def index
+    render plain: '/root'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: "application#index"
   scope '/api/v1', defaults: {format: :json} do
     resources :articles
+    devise_for :users
   end
 end
